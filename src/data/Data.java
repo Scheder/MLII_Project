38b95f6cf8842e2a4b79d078e016ce084d653f03
SimSla <simsla@ulyssis.org>
2014-12-10 22:39:50 +0100

    dd

--- src/data/Data.java
+++ src/data/Data.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
 
 import weka.core.Attribute;
 import weka.core.Instance;
