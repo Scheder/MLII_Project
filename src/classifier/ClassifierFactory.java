9880dc999037e610bf978d310a914b981178dea5
SimSla <simsla@ulyssis.org>
2014-12-11 03:01:29 +0100

    Merge branch 'master' into sandbox
    Conflicts:
    .classpath
    src/data/Data.java

--- src/classifier/ClassifierFactory.java
+++ src/classifier/ClassifierFactory.java
@@ -1,12 +1,24 @@
 package classifier;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import weka.classifiers.Classifier;
+import weka.classifiers.meta.Vote;
+import weka.classifiers.mi.MISVM;
+import weka.classifiers.trees.J48;
+import weka.core.Attribute;
+import weka.core.FastVector;
+import weka.core.Instance;
+import weka.core.Instances;
 import codebook.Codebook;
 import codebook.CodebookFactory;
 import data.FrameSet;
 
 public class ClassifierFactory {
 	
-	public static void createClassifier(FrameSet labeled, FrameSet unlabeled){
+	//TODO handle exception
+	public static void createClassifier(FrameSet labeled, FrameSet unlabeled) throws Exception{
 		
 		// Fast code book learning.
 		// TODO: choose values, or make value picker.
@@ -25,6 +37,46 @@ public class ClassifierFactory {
 		// add to classifier with labeled vector
 		
 		// C = train classifier
-		// return C.
+		int numOfFrames = labeled.size();
+		int numOfBasicVectors = labeled.dimension();
+		FastVector attributes = new FastVector(numOfBasicVectors);
+		//Create attributes for all basic vectors
+		for (int i = 0; i < numOfBasicVectors; i++) {
+			attributes.addElement(new Attribute(""+i));
+		}
+		//TODO make createClassifier more generic, so it can be reused for gait recognition
+		attributes.addElement("walking");
+		
+		//Create training set
+		Instances trainingSet = new Instances("Rel",attributes,numOfFrames);
+		trainingSet.setClassIndex(numOfBasicVectors);
+		for (int i = 0; i < numOfFrames; i++) {
+			//TODO get label.
+			double[] attValues = labeled.getFrame(i).toArray();
+			Instance instance = new Instance(1, attValues);
+			trainingSet.add(instance);
+		}
+		
+		
+		//Create array of classifiers
+		List<Classifier> classifiers = new ArrayList<Classifier>();
+		//TODO use options for classifiers and add classifiers
+		Classifier svm = new MISVM();
+		Classifier j48 = new J48();
+		
+		classifiers.add(svm);
+		classifiers.add(j48);
+		
+		//TODO use correct options for (meta)classifiers
+		Vote vote = new Vote();
+		vote.setClassifiers((Classifier[])classifiers.toArray());
+		
+		vote.buildClassifier(trainingSet);
+		// return return vote.
+	}
+	
+	public static void testClassifier(Classifier classifier, FrameSet testSet) {
+		//Transform FrameSet to Instances
+		//TODO get statistics, confusion matrix etc.
 	}
 }
--- src/data/Data.java
+++ src/data/Data.java
@@ -1,12 +1,29 @@
 package data;
 
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Vector;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.plaf.FileChooserUI;
 
 import org.apache.commons.math3.linear.ArrayRealVector;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
 
 import codebook.CodebookFactory;
 import weka.core.Attribute;
@@ -18,20 +35,20 @@ import weka.core.converters.CSVLoader;
 
 public class Data {
 	
-	private static final Double windowSize = 1.0; //In seconds
-	private static final Double overlap = 0.5; //In percentage of window overlap 
-	private static final Double sampleRate = 50.0; //In Hz
+	//All windows are assumed the same size
+	private static final int sampleRate = 50; //In Hz
+	private static final int windowSize = 1 * Data.sampleRate; //Number of instances in window
+	private static final Double overlap = 0.5; //In percentage of window overlap
+	private static final int instancesBetweenWindows = ((Double)((1-Data.overlap) * Data.windowSize)).intValue();
 	
 	private Instances instances;
-	private List<Integer> windowFromIndices;
-	private List<Integer> windowToIndices;
-	private boolean changed;
 	
 	private Data() {
 		this.instances = null;
-		this.windowFromIndices = new ArrayList<Integer>();
-		this.windowToIndices = new ArrayList<Integer>();
-		this.changed = true;
+	}
+	
+	private Data(Instances instances) {
+		this.instances = instances;
 	}
 	
 	public static Data readCSV (final String file) throws IOException {
@@ -69,7 +86,6 @@ public class Data {
 		d.instances.insertAttributeAt(walking,d.instances.numAttributes());
 		d.instances.setClass(walking);
 		
-		d.changed = true;
 		return d;
 	}
 	
@@ -85,45 +101,19 @@ public class Data {
 		saver.writeBatch();
 	}
 	
+	/**
+	 * Get window with given index
+	 * @param index - Index starts at 0
+	 * @return
+	 */
 	public Data getWindow(final int index) {
-		if (this.changed) this.getWindowIndices();
-		
-		Data d = new Data();
-		final int fromIndex = this.windowFromIndices.get(index);
-		final int toIndex = this.windowToIndices.get(index);
-		
-		d.instances = new Instances(this.instances, fromIndex, (toIndex-fromIndex));
-		
-		return d;
+		final int fromIndex = Data.instancesBetweenWindows * index;
+		return new Data(new Instances(this.instances, fromIndex, Data.windowSize));
 	}
 	
 	public int numOfWindows() {
-		if (this.changed) this.getWindowIndices();
-		return this.windowFromIndices.size();
-	}
-	
-	/**
-	 * For now this function assumes Data only contains the data of one file,
-	 * for which is assumed that the data has samplerate instances per second,
-	 * without time hiatuses.
-	 */
-	private void getWindowIndices() {
-		int windowSize = ((Double)(Data.sampleRate * Data.windowSize)).intValue();
-		int overlapSize = new Double((1.0-Data.overlap) * windowSize).intValue();
-		
-		int fromIndex,toIndex;
-		for (fromIndex = 0, toIndex = windowSize; toIndex < this.instances.numInstances();
-				fromIndex += overlapSize, toIndex += overlapSize) {
-			this.windowFromIndices.add(fromIndex);
-			this.windowToIndices.add(toIndex);
-		}
-		
-		if (fromIndex < this.instances.numInstances()) {
-			this.windowFromIndices.add(fromIndex);
-			this.windowToIndices.add(this.instances.numInstances());
-		}
-		
-		this.changed = false;
+		if (this.instances.numInstances() < Data.windowSize) return 0;
+		return (this.instances.numInstances() - Data.windowSize) / Data.instancesBetweenWindows + 1;
 	}
 	
 	/**
@@ -138,10 +128,9 @@ public class Data {
 		//Iterate windows
 		for (int i = 0; i < size; i++) {
 			Data window = this.getWindow(i);
-			int windowsize = window.instances.numInstances();
-			ArrayRealVector vector = new ArrayRealVector(windowsize);
+			ArrayRealVector vector = new ArrayRealVector(Data.windowSize);
 			//Iterate instances in window
-			for (int j = 0; j < windowsize; j++) {
+			for (int j = 0; j < Data.windowSize; j++) {
 				vector.setEntry(j, window.instances.instance(j).value(magAttr));
 			}
 			list.add(vector);
@@ -151,15 +140,139 @@ public class Data {
 		return list;
 	}
 	
-	public static void main(String[] args) throws IOException {
-		Data d = Data.readCSV("Project/train/walk_5_other.csv");
-		d.toArff("test.arff");
-		//System.out.println(d.instances);
-		//System.out.println(d.toArrayRealVector());
-		FrameSet frameSet = new FrameSet(d.toArrayRealVector(), sampleRate.intValue());
-		System.out.println(frameSet.size());
+	@Override
+	public String toString() {
+		String str = "";
+		str += "[windowsize:"+Data.windowSize;
+		str += ",overlap:"+Data.overlap;
+		str += ",numberOfInstances:"+this.instances.numInstances();
+		str += ",numberOfWindows:"+this.numOfWindows();
+		str += ",instances:\n"+this.instances;
+		return str;
+	}
+	
+	public void visualize() {
+		ApplicationFrame frame = new ApplicationFrame("Data");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new GridLayout(2,2));
 		
-		CodebookFactory.newCodebook(frameSet, "partitionSize", 20, 40, 10, 0.5);
+		//Define the data series
+		final XYSeries x_values = new XYSeries("x");
+		final XYSeries y_values = new XYSeries("y");
+		final XYSeries z_values = new XYSeries("z");
+		final XYSeries m_values = new XYSeries("m");
+		
+		for (int i = 0; i < this.instances.numInstances(); i++) {
+			Instance instance = this.instances.instance(i);
+			Double t = instance.value(0)/1e9;
+			x_values.add(t,(Double)instance.value(1));
+			y_values.add(t,(Double)instance.value(2));
+			z_values.add(t,(Double)instance.value(3));
+			m_values.add(t,(Double)instance.value(4));
+		}
+		
+		//Define the collection for the data series
+		final XYSeriesCollection x_data = new XYSeriesCollection();
+		final XYSeriesCollection y_data = new XYSeriesCollection();
+		final XYSeriesCollection z_data = new XYSeriesCollection();
+		final XYSeriesCollection m_data = new XYSeriesCollection();
+		x_data.addSeries(x_values);
+		y_data.addSeries(y_values);
+		z_data.addSeries(z_values);
+		m_data.addSeries(m_values);
+		
+		//Add the series to a chart
+		final JFreeChart x_chart = ChartFactory.createXYLineChart(
+				"X","time","acceleration",x_data,PlotOrientation.VERTICAL,
+				false,true,false);
+		final JFreeChart y_chart = ChartFactory.createXYLineChart(
+				"Y", "time", "acceleration",y_data,PlotOrientation.VERTICAL,
+				false,true,false);
+		final JFreeChart z_chart = ChartFactory.createXYLineChart(
+				"Z","time","acceleration",z_data,PlotOrientation.VERTICAL,
+				false,true,false);
+		final JFreeChart m_chart = ChartFactory.createXYLineChart(
+				"Magnitude", "time", "acceleration",m_data,PlotOrientation.VERTICAL,
+				false,true,false);
+				
+		//Add the charts to panels and the panels to the frame
+		final ChartPanel x_panel = new ChartPanel(x_chart);
+		final ChartPanel y_panel = new ChartPanel(y_chart);
+		final ChartPanel z_panel = new ChartPanel(z_chart);
+		final ChartPanel m_panel = new ChartPanel(m_chart);
+		x_panel.setPreferredSize(new Dimension(500,270));
+		y_panel.setPreferredSize(new Dimension(500,270));
+		z_panel.setPreferredSize(new Dimension(500,270));
+		m_panel.setPreferredSize(new Dimension(500,270));
+		frame.add(x_panel);
+		frame.add(y_panel);
+		frame.add(z_panel);
+		frame.add(m_panel);
+		RefineryUtilities.centerFrameOnScreen(frame);
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	/**
+	 * Asks what file to visualize, then visualizes the file
+	 * @throws IOException 
+	 */
+	public static void visualizeFile() throws IOException {
+		final ApplicationFrame frame = new ApplicationFrame("Data");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JButton button = new JButton("Browse...");
+		button.addActionListener( new ActionListener() {
+			
+			private File currentDirectory = new File(System.getProperty("user.dir"));
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser fileChooser = new JFileChooser();
+				fileChooser.setCurrentDirectory(currentDirectory);
+				int result = fileChooser.showOpenDialog(frame);
+				
+				if (result == JFileChooser.APPROVE_OPTION) {
+					currentDirectory = fileChooser.getCurrentDirectory();
+					Data data;
+					try {
+						data = Data.readCSV(fileChooser.getSelectedFile());
+						data.visualize();
+					} catch (IOException e1) {
+						JOptionPane.showMessageDialog(frame, e1.getMessage());
+						e1.printStackTrace();
+					}
+				}
+			}
+		});
+		
+		frame.add(button);
+		
+		RefineryUtilities.centerFrameOnScreen(frame);
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	public static void main(String[] args) throws IOException {
+//		Data d = Data.readCSV("Project/train/walk_5_other.csv");
+//		d.toArff("test.arff");
+//		//System.out.println(d.instances);
+//		//System.out.println(d.toArrayRealVector());
+//		FrameSet frameSet = new FrameSet(d.toArrayRealVector(), sampleRate.intValue());
+//		System.out.println(frameSet.size());
+//		
+//		CodebookFactory.newCodebook(frameSet, "partitionSize", 20, 40, 10, 0.5);
+
+
+//		Data d = Data.readCSV("Project/train/walk_3_other.csv");
+//		d.toArff("test.arff");
+//		System.out.println(d);
+//		System.out.println(d.toArrayRealVector());
+//		d.visualize();
+//		for (int i = 0; i < d.numOfWindows(); i++) {
+//			System.out.println(d.getWindow(i));
+//		}
+		Data.visualizeFile();
 	}
 	
 }
