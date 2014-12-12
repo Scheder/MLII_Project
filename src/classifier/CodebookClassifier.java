99dfdcf108ae824eff16c8a59b9a3e79b33fbbf4
SimSla <simon.slangen@gmail.com>
2014-12-12 12:06:57 +0100

    Regenerate, comments, print margin

--- src/classifier/CodebookClassifier.java
+++ src/classifier/CodebookClassifier.java
@@ -16,16 +16,24 @@ public class CodebookClassifier {
 	private Codebook codebook;
 	private Classifier classifier;
 	
-	public CodebookClassifier(final Codebook codebook, final Classifier classifier) {
+	public CodebookClassifier(final Codebook codebook, 
+			final Classifier classifier) {
 		this.codebook = codebook;
 		this.classifier = classifier;
 	}
 	
 	public List<String> getLabels(final Data d) throws Exception {
 		//Get activations for all frames
-		FrameSet activations = this.codebook.activate(new FrameSet(d.toArrayRealVector()));
-		Instances instances = ClassifierFactory.activationsToInstances(activations,d.getClassName(),d.getClassValues());
-		final List<String> labels = new ArrayList<String>(instances.numInstances());
+		FrameSet activations = 
+				this.codebook.activate(new FrameSet(d.toArrayRealVector()));
+		
+		Instances instances = 
+				ClassifierFactory.activationsToInstances(activations,
+						d.getClassName(),d.getClassValues());
+		
+		final List<String> labels = 
+				new ArrayList<String>(instances.numInstances());
+		
 		for (int i = 0; i < instances.numInstances(); i++) {
 			Instance instance = instances.instance(i);
 			int classId = (int)this.classifier.classifyInstance(instance);
